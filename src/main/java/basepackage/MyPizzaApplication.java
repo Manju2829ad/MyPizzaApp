89f7568cf58d4f4bae69c6d5dee3d80c23c32d4d
Manju2829ad <jasonjackie987@gmail.com>
2025-03-14 18:52:22 +0530

    remoed hte comments

--- src/main/java/basepackage/MyPizzaApplication.java
+++ src/main/java/basepackage/MyPizzaApplication.java
@@ -11,8 +11,10 @@ public class MyPizzaApplication {
 
 	public static void main(String[] args) {
 		
-		System.out.println("DB URL: " + System.getenv("RENDER_DB_URL"));
-	    System.out.println("DB Username: " + System.getenv("DB_USER"));
+		
+		
+//		System.out.println("DB URL: " + System.getenv("RENDER_DB_URL"));
+//	    System.out.println("DB Username: " + System.getenv("DB_USER"));
 	    
 	    
 		SpringApplication.run(MyPizzaApplication.class, args);
